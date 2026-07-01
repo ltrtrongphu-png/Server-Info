ba85578bb68f3579f8c332cff5a9cc198c4a2e29
Nhạt <ltrtrongphu@gmail.com>
2026-07-01 09:20:38 +0700

    Update ServerInfoMod.java

--- ServerInfo/serverinfo/src/main/java/com/example/serverinfo/ServerInfoMod.java
+++ ServerInfo/serverinfo/src/main/java/com/example/serverinfo/ServerInfoMod.java
@@ -1,1 +1,11 @@
+package com.example.serverinfo;
 
+import net.fabricmc.api.ClientModInitializer;
+
+public class ServerInfoMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("[ServerInfo] Mod loaded! Nhan !server de xem thong tin.");
+    }
+}
+mixin/ServerInfoMixin.java
